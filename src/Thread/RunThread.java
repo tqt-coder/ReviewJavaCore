2acb9c7e09101bd369aee7b6abfafbd4f68563bf
tranquoctuan2001 <76949884+tranquoctuan2001@users.noreply.github.com>
2022-05-25 09:02:00 +0700

    edit thread

--- src/Thread/RunThread.java
+++ src/Thread/RunThread.java
@@ -6,29 +6,17 @@ public class RunThread extends Thread {
 
     @Override
     public void run() {
-//        IntStream.rangeClosed(0, 10).boxed().forEach(x->{
-//            System.out.println("Run child thread with "+ ++x);
-//
-//            try {
-//                Thread.sleep(1030);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//        System.out.println("  - ==> Child thread stopped");
-
-        int index = 1;
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("  - HelloThread running " + index++);
+        IntStream.rangeClosed(0, 10).boxed().forEach(x->{
+            System.out.println("     + Run child thread with "+ ++x);
 
             try {
-                // Ngủ 1030 milli giây.
                 Thread.sleep(1030);
             } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+        });
+        System.out.println(" ==> Child thread stopped");
 
-        }
-        System.out.println("  - ==> HelloThread stopped");
+
     }
 }
